1212fd5e65004aa19d3efb46695483ade4af08a4
povk35808 <povk35808@gmail.com>
2025-11-04 21:28:55 +0700

    Create khmer_font_base64.js

---
+++ khmer_font_base64.js
@@ -0,0 +1,12 @@
+// This file makes the Khmer font available to our app
+if (typeof MySokhaApp === 'undefined') {
+  var MySokhaApp = {};
+}
+
+/* សូមបិទភ្ជាប់ (Paste) កូដ Base64 ដ៏វែង 
+  ដែលអ្នកបានចម្លងពីជំហានទី២ មកជំនួសកន្លែងនេះ
+*/
+MySokhaApp.khmerFontBase64 = "PASTE_YOUR_LONG_BASE64_STRING_HERE";
+
+
+console.log("Khmer Font Base64 Loaded.");
